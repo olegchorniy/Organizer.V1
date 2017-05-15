707789649518b5ec9377ddd4a4f7f03dc8798f74
Oleg <alig007@mail.ru>
2017-05-15 19:00:49 +0300

    Events service module added.

---
+++ events-service/src/main/java/kpi/ipt/organizer/events/EventsServiceApplication.java
@@ -0,0 +1,12 @@
+package kpi.ipt.organizer.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsServiceApplication.class, args);
+	}
+}
