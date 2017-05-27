ea29c9442aeb5e9f1cf593ef9158fefc643347c1
Oleg <alig007@mail.ru>
2017-05-27 21:30:09 +0300

    Spring actuator and healthchecks added to the events service. Error fixed in startup.bat file.

--- startup.bat
+++ startup.bat
@@ -3,6 +3,9 @@ timeout 3 > NUL
 
 start java -jar user-service\target\user-service-0.0.1-SNAPSHOT.jar
 
+REM Start with externally configured Eureka server URL.
+REM java -jar -Deureka.client.service_url.defaultZone=http://localhost:8761/eureka/ user-service\target\user-service-0.0.1-SNAPSHOT.jar
+
 start mongod --port 27111 --dbpath D:\servers\mongodb_data_directory\notes_db_sa
 timeout 2 > NUL
 
