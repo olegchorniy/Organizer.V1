16ed293477d7e0fa47240ab39d527ebdec4affbb
Oleg <alig007@mail.ru>
2017-05-31 22:10:48 +0300

    Frontend service became distributed with Hazelcast as distributed session storage.

---
+++ frontend/src/main/java/kpi/ipt/organizer/frontend/config/HazelcastSessionConfig.java
@@ -0,0 +1,37 @@
+package kpi.ipt.organizer.frontend.config;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.MapAttributeConfig;
+import com.hazelcast.config.MapIndexConfig;
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.session.hazelcast.HazelcastSessionRepository;
+import org.springframework.session.hazelcast.PrincipalNameExtractor;
+
+@Configuration
+public class HazelcastSessionConfig {
+
+    @Bean(destroyMethod = "shutdown")
+    public HazelcastInstance hazelcastInstance() {
+
+        MapAttributeConfig attributeConfig = new MapAttributeConfig()
+                .setName(HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
+                .setExtractor(PrincipalNameExtractor.class.getName());
+
+        MapIndexConfig indexConfig = new MapIndexConfig(
+                HazelcastSessionRepository.PRINCIPAL_NAME_ATTRIBUTE,
+                false
+        );
+
+        Config config = new Config();
+
+        config.getMapConfig("spring:session:sessions")
+                .addMapAttributeConfig(attributeConfig)
+                .addMapIndexConfig(indexConfig);
+
+        return Hazelcast.newHazelcastInstance(config);
+    }
+
+}
